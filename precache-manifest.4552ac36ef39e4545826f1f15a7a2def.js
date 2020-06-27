4fd076f866979eacc239ae2143edd844ea4374f8
Ekrem TAŞ <ekremtas589@gmail.com>
2020-06-27 17:55:30 +0300

    Updates

---
+++ precache-manifest.4552ac36ef39e4545826f1f15a7a2def.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86ddd0e3af5d74aa8de99e7d43c158b3",
+    "url": "/fazlagidalivecode/index.html"
+  },
+  {
+    "revision": "f063d17efacc5555898c",
+    "url": "/fazlagidalivecode/static/css/2.92a0e85f.chunk.css"
+  },
+  {
+    "revision": "528b3f135c1108c904e8",
+    "url": "/fazlagidalivecode/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f063d17efacc5555898c",
+    "url": "/fazlagidalivecode/static/js/2.ec45edaa.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/fazlagidalivecode/static/js/2.ec45edaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "528b3f135c1108c904e8",
+    "url": "/fazlagidalivecode/static/js/main.93b9a2e1.chunk.js"
+  },
+  {
+    "revision": "07892605b31fef9c1ecf",
+    "url": "/fazlagidalivecode/static/js/runtime-main.39243b34.js"
+  }
+]);
