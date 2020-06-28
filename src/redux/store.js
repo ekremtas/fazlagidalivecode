e8e1b419b4faebcef74689a0421a0c2bc168b48d
Ekrem TAŞ <ekremtas589@gmail.com>
2020-06-28 21:34:44 +0300

    react-devtools-extension library Added and Used.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,14 @@
 import { combineReducers, createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { chartsReducer } from "./reducers";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const reducers = combineReducers({
   chartsReducer,
 });
 
-const store = createStore(reducers, applyMiddleware(thunk));
+const store = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 export default store;
